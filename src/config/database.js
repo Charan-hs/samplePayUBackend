f1f41d8e7214001e21ac6a02408f14f61d884452
Charan H S <charanengg08@gmail.com>
2022-03-18 13:40:07 +0530

    data base update

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,5 @@
   const data = {
-    database : "mongodb+srv://admin:admin@cluster0.o8vvw.gcp.mongodb.net/pam?retryWrites=true&w=majority"
+    database : "mongodb+srv://admin:admin@cluster0.o8vvw.gcp.mongodb.net/payu?retryWrites=true&w=majority"
    }
 
 export const database = data.database
